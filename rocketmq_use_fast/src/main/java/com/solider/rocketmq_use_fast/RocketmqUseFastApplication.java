7a7dbb52c5a8c686a50181d9e5f7319e17be5446
wzh <2460316849@qq.com>
2024-07-23 22:31:09 +0800

    first commit

---
+++ rocketmq_use_fast/src/main/java/com/solider/rocketmq_use_fast/RocketmqUseFastApplication.java
@@ -0,0 +1,13 @@
+package com.solider.rocketmq_use_fast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqUseFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqUseFastApplication.class, args);
+    }
+
+}
---
+++ rocketmq_use_fast/src/test/java/com/solider/rocketmq_use_fast/RocketmqUseFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solider.rocketmq_use_fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqUseFastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
